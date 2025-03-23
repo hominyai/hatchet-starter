b67c7476eed97f9a46dc3eeeeee7fde7f588e2bc
Kyle Smith <kyle@hominy.ai>
2025-03-22 20:07:45 -0500

    fix: use correct path to config file during token creation

--- packages/hatchet/docker/init-local.sh
+++ packages/hatchet/docker/init-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 
 : ${X_HATCHET_CLIENT_SECRETS_DIR?"X_HATCHET_CLIENT_SECRETS_DIR must be set"}
 
@@ -7,10 +7,10 @@ mkdir -p "${X_HATCHET_CLIENT_SECRETS_DIR}"
 
 hatchet_client_token_file="${X_HATCHET_CLIENT_SECRETS_DIR}/hatchet_client_token"
 
-if ! [ -f "${hatchet_client_token_file}" ]; then
+if ! [ -f "${hatchet_client_token_file}" ] || [ -s "${hatchet_client_token_file}" ]; then
   ## adapted from https://github.com/hatchet-dev/hatchet/blob/v0.53.14/frontend/docs/pages/self-hosting/hatchet-lite.mdx?plain=1#L142-L143
   ten_years_in_hours="$(expr 10 '*' 365 '*' 24)"
-  hatchet-admin --config /config \
+  hatchet-admin --config /opt/hatchet/config \
     token create \
       --tenant-id 00000000-0000-0000-0000-000000000000 --expiresIn "${ten_years_in_hours}h0m0s" \
     | tee "${hatchet_client_token_file}"
